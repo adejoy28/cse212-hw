1d9a8e86d3630769da70d1e22e85ded6cbd05c0e
John Adebayo <johnayomide31@gmail.com>
2026-05-09 14:28:27 +0100

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,20 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // To solve this,
+        // I will create an empty <list> Array
+        // next, I will loop my operation in accordance to the length provide as parament (arg2)
+        // while creating an index variable to check and not overloop or underloop
+        // for each index, i will add the result to the operation to the array initially created
+        // I will return the list of array, now filled with data. 
+        List<double> MultiplesOfArray = [];
+
+        for (int index = 1; index <= length; index++)
+        {
+            MultiplesOfArray.Add(number * index);
+        }
+
+        return MultiplesOfArray.ToArray();
     }
 
     /// <summary>
@@ -29,5 +42,24 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // To solve this
+        // First, I will find the position (index) where the rotation starts, and save it (startIndex)
+        // next, I will select and save the items meant to be rotated from the startIndex to the end in a temporary list
+        // I will remove those elements from the end of the original list
+        // and I will insert them at the beginning of the original list
+
+        // Find the position to start the rotation
+        int startIndex = data.Count - amount;
+
+        // get the range of items from the original data starting from the startIndex and the amount
+        List<int> lastItems = data.GetRange(startIndex, amount);
+
+        // Remove the range from the original data
+        data.RemoveRange(startIndex, amount);
+
+        // Insert into the original data in the beginning
+        data.InsertRange(0, lastItems);
+
     }
 }
